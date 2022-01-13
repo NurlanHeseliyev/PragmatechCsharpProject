a0d7181a0879193380ce7f2151bed0d4414672e2
Heseliyev Nurlan <heseliyevnurlan14@gmail.com>
2022-01-13 21:20:18 +0400

    class

---
+++ lesson/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudent
+{
+    class student
+    {
+        public string fullname;
+        public string GrupNo;
+        public int AGE;
+        public student(string Fullname, string grupNo,int age)
+        {
+            fullname = Fullname;
+            GrupNo = grupNo;
+            AGE= age;
+        }
+        public void GetBirthYear(student student)
+        {
+            int year = DateTime.Now.Year - student.AGE;
+            Console.WriteLine(year);
+            Console.ReadLine();
+        }
+    }
+}
